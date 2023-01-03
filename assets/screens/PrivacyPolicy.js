96076e94d767ba78c0549d8008d3437492782e97
Geoffrey zimba <jayzimba40@gmail.com>
2023-01-03 02:44:16 +0200

    third commit

--- assets/screens/PrivacyPolicy.js
+++ assets/screens/PrivacyPolicy.js
@@ -29,11 +29,10 @@ export default function PrivacyPolicy() {
       </TouchableOpacity>
       <ScrollView
         showsVerticalScrollIndicator="false"
-        style={{ marginBottom: 80, paddingBottom:30 }}
+        style={{ marginBottom: 80, paddingBottom: 30 }}
       >
         <View style={{ alignItems: "center", marginVertical: 20 }}>
           <MaterialIcons name="policy" size={50} color={COLORS.gray} />
-
           <Text
             style={{
               marginHorizontal: 20,
@@ -48,31 +47,31 @@ export default function PrivacyPolicy() {
             “on mission”, in a normal village bank about 50% of all new members
             entering the program will be severely poor—representing families
             with a daily per-capita expenditure (DPCE) of less than US$1; the
-            rest are moderately poor (DPCE=$1–2/day) or non-poor (DPCE
-            >$2).[citation needed] These women meet once a week in the home of
-            one of their members to avail themselves of working capital loans, a
-            safe place to save, skill training, mentoring, and motivation. Loans
-            normally start at $50–$100 and are linked to savings such that the
-            more a client saves the more she can borrow. The normal loan period
-            is four months and is repaid in 16 weekly installments.[1] At the
-            end of 2006, 95% of clients covered by a benchmark sample of 71 NGOs
-            and institutions engaged in village bank lending were women.[2] To
-            eliminate the need for collateral (the poor man's obstacle to
-            receiving bank loans), village banks rely on a variation of the
-            solidarity lending methodology. It relies on a system of
-            cross-guarantees, where each member of a village bank ensures the
-            loan of every other member. This system gives rise to an atmosphere
-            of social pressure within the village bank, where the cost of social
-            embarrassment motivates bank members to repay their loans in full.
-            The admixture of cross-guarantees and social pressure makes it
-            possible for even the poorest people to receive loans. This method
-            has proven very effective for FINCA, yielding a repayment rate of
-            over 97% in its worldwide network. Village banks are highly
-            democratic, self-managed, grassroots organizations. They elect their
-            own leaders, select their own members, create their own bylaws, do
-            their own bookkeeping, manage all funds, disburse and deposit all
-            funds, resolve loan delinquency problems, and levy their own fines
-            on members who come late, miss meetings, or fall behind in their
+            rest are moderately poor or non-poor (DPCE >$2).[citation needed]
+            These women meet once a week in the home of one of their members to
+            avail themselves of working capital loans, a safe place to save,
+            skill training, mentoring, and motivation. Loans normally start at
+            $50–$100 and are linked to savings such that the more a client saves
+            the more she can borrow. The normal loan period is four months and
+            is repaid in 16 weekly installments.[1] At the end of 2006, 95% of
+            clients covered by a benchmark sample of 71 NGOs and institutions
+            engaged in village bank lending were women.[2] To eliminate the need
+            for collateral (the poor man's obstacle to receiving bank loans),
+            village banks rely on a variation of the solidarity lending
+            methodology. It relies on a system of cross-guarantees, where each
+            member of a village bank ensures the loan of every other member.
+            This system gives rise to an atmosphere of social pressure within
+            the village bank, where the cost of social embarrassment motivates
+            bank members to repay their loans in full. The admixture of
+            cross-guarantees and social pressure makes it possible for even the
+            poorest people to receive loans. This method has proven very
+            effective for FINCA, yielding a repayment rate of over 97% in its
+            worldwide network. Village banks are highly democratic,
+            self-managed, grassroots organizations. They elect their own
+            leaders, select their own members, create their own bylaws, do their
+            own bookkeeping, manage all funds, disburse and deposit all funds,
+            resolve loan delinquency problems, and levy their own fines on
+            members who come late, miss meetings, or fall behind in their
             payments.[1] There was some hope in the early years of village bank
             development that these small village organizations could become
             independent and self-financing, but this hope was later abandoned.
